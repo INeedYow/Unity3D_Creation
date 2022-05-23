603c0e186d7e8f5752587ac0fc64e021fcfe8242
INeedYow <zmfhshtm258@naver.com>
2022-05-23 16:30:07 +0900

    스킬 구현 수정 전 백업

--- Assets/Scripts/Skill/Skill.cs
+++ Assets/Scripts/Skill/Skill.cs
@@ -7,9 +7,9 @@ public class Skill : MonoBehaviour
     public SkillData data;
 
     [HideInInspector] public Character  owner;
-    [HideInInspector] public int ID;                  // 몇 번째 스킬인지
-    public SkillCommand     skillcommand;
-    public EffectCommand    effCommand;
+    [HideInInspector] public int ID;                    // 몇 번째 스킬인지
+    public SkillCommand     skillcommand;               // 스킬 사용 입력받았을 때 명령(즉시 사용, 타겟에게 이동 후 사용 등)
+    public EffectCommand    effCommand;                 // 스킬 발동 타이밍에 처리(애니메이션에서 GFX 통해서 호출)
 
     private void Awake() { SetCommand(); }
     void SetCommand(){ 
